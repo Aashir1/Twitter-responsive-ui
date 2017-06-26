eb54b8f9618381035528e4948fd2373fab0456e3
Aashir <kaashir9@gmail.com>
2017-06-26 18:09:54 +0500

    Twitter-resonsive-ui

---
+++ app.js
@@ -0,0 +1,7 @@
+function toggleFunction() {
+    document.getElementById("nav-ul").classList.toggle("showing");
+    document.getElementById('after1').classList.toggle('after');
+    document.getElementById('after2').classList.toggle('after');
+    document.getElementById('after3').classList.toggle('after');
+  
+}
